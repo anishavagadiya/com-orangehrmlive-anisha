791dc02feb5dc0a0b4181a21f81bc7c2ff666655
anishavagadiya <vagadiya11@gmail.com>
2022-12-06 18:54:57 +0000

    orangehrmlivemaven-anisha

---
+++ src/test/java/com/orangehrmlive/demo/testsuite/ForgotPasswordTest.java
@@ -0,0 +1,21 @@
+package com.orangehrmlive.demo.testsuite;
+
+import com.orangehrmlive.demo.pages.ForgotPasswordPage;
+import com.orangehrmlive.demo.pages.LoginPage;
+import com.orangehrmlive.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ForgotPasswordTest extends BaseTest {
+    LoginPage loginPage = new LoginPage();
+    ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
+
+    @Test
+    public void userShouldNavigateToForgotPasswordPageSuccessfully(){
+        loginPage.clickOnForgotPasswordLink();
+
+        String expectedErrorMessage = "Reset Password";
+        Assert.assertEquals(forgotPasswordPage.getErrorMessage(),expectedErrorMessage,"Reset password text is not displayed");
+    }
+
+}
